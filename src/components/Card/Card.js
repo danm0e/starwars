3afc5bfbbf8edd61332e36a2bc5459b58a2e5c26
Christopher Grace <chris.grace@designinc.co.uk>
2021-05-15 00:07:56 +0100

    Add dynamic props

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,23 +3,30 @@ import PropTypes from 'prop-types'
 
 import { Wrapper, Header, Content, Title } from './styles'
 
-/** Primary UI component for displaying preview data */
-const Card = ({ data }) => {
-  console.log(data)
-
+/** Wrapper component for card data */
+const Card = ({ title, image, url, onClick }) => {
   return (
-    <Wrapper aria-label='Card'>
-      <Header src='https://via.placeholder.com/300x200' alt='' />
+    <Wrapper aria-label='Card' onClick={onClick}>
+      <Header {...image} />
       <Content>
-        <Title>Luke SkyWalker</Title>
+        <Title>{title}</Title>
       </Content>
     </Wrapper>
   )
 }
 
 Card.propTypes = {
-  /** Main data object for this card */
-  data: PropTypes.object.isRequired,
+  /** Title of card */
+  title: PropTypes.string.isRequired,
+  /** Image of card */
+  image: PropTypes.shape({
+    src: PropTypes.string,
+    alt: PropTypes.string,
+  }),
+  /** URL of card */
+  url: PropTypes.string.isRequired,
+  /** Click event */
+  onClick: PropTypes.func.isRequired,
 }
 
 export default Card
